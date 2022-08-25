b38258ef3c2992f9891daea94ad3328454aea012
Naga Raju <77769005+GARAGANAGARAJU@users.noreply.github.com>
2022-08-25 09:42:16 +0530

    Update terraformi.tf

--- terraformi.tf
+++ terraformi.tf
@@ -1,7 +1,8 @@
 provider "aws" {
-  region = "us-east-1"
+  region     = "us-west-2"
+  access_key = secrets.AWS_ACCESS_KEY_ID
+  secret_key = secrets.AWS_SECRET_ACCESS_KEY
 }
-
 resource "aws_vpc" "vpc" {
   cidr_block = "10.0.0.0/16"
 }
